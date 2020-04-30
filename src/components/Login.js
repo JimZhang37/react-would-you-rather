106e2819f4ceeec1afd55d446181edafc77ea792
ZHANG Yaohua <zhangyaohua40605@gmail.com>
2020-04-30 10:13:23 +0800

    change login from function to class and replace hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,61 @@
-import React, { useState } from "react";
+import React, {  Component } from "react";
 import { connect } from "react-redux";
 import { handleAuthUser } from "../actions/authedUser";
-import { useHistory, useLocation } from 'react-router-dom'
+import {withRouter } from 'react-router-dom'
 
-function Login({ usersArray, authedUser, dispatch }) {
-    let history = useHistory();
-    let location = useLocation();
-    const [user, setUser] = useState('');
-    let { from } = location.state || { from: { pathname: "/" } };
-    
-    function login(e) {
-        dispatch(handleAuthUser(user))
-        history.replace(from);
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: '',
+            from: props.location.state.from || { pathname: "/" } 
+        }
+    }
+
+    login(e) {
+        this.props.dispatch(handleAuthUser(this.state.user))
     };
 
-    if (authedUser !== null) {
-        history.replace(from)
+    handleChange(e) {
+        this.setState({ user: e.target.value })
     }
-    return (
-        <div>
-            <select value={user} onChange={(e) => (setUser(e.target.value))}>
-                <option value="" disabled>Select your option</option>
-                {usersArray.map(user => <option value={user.id} key={user.id}>{user.name}</option>)}
-            </select>
-            <button onClick={(e)=>login(e)}>Login In</button>
-        </div>)
-
+    render() {
+        const {history,  usersArray, authedUser} = this.props
+        if (authedUser !== null) {
+            history.replace(this.state.from)
+        }
+        return (
+            <div>
+                <select value={this.state.user} onChange={(e) => (this.handleChange(e))}>
+                    <option value="" disabled>Select your option</option>
+                    {usersArray.map(user => <option value={user.id} key={user.id}>{user.name}</option>)}
+                </select>
+                <button onClick={(e) => this.login(e)}>Login In</button>
+            </div>)
+    }
 }
+
+// function Login({ usersArray, authedUser, dispatch }) {
+    // let history = useHistory();
+    // let location = useLocation();
+    // const [user, setUser] = useState('');
+    // let { from } = location.state || { from: { pathname: "/" } };
+
+
+
+//     if (authedUser !== null) {
+//         history.replace(from)
+//     }
+//     return (
+//         <div>
+//             <select value={user} onChange={(e) => (handleChange(e))}>
+//                 <option value="" disabled>Select your option</option>
+//                 {usersArray.map(user => <option value={user.id} key={user.id}>{user.name}</option>)}
+//             </select>
+//             <button onClick={(e) => login(e)}>Login In</button>
+//         </div>)
+
+// }
 
 
 function mapStateToProps({ users, authedUser }) {
@@ -38,4 +67,4 @@ function mapStateToProps({ users, authedUser }) {
     }
 }
 
-export default connect(mapStateToProps)(Login)
+export default withRouter(connect(mapStateToProps)(Login))
